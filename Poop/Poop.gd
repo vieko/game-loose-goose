fa9efb3735e167a4e28be2a737b4d8cd44ff0a9a
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2023-08-19 21:12:31 -0600

    improves poop splashes

--- Poop/Poop.gd
+++ Poop/Poop.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var plPoopSplash := preload("res://Poop/PoopSplash.tscn")
+
 @export var speed: float = 500
 
 func _physics_process(delta):
@@ -11,5 +13,9 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 func _on_area_entered(area):
   if area.is_in_group(Globals.Groups.DAMAGEABLES):
+    var poopSplash := plPoopSplash.instantiate()
+    poopSplash.position = position
+    get_parent().add_child(poopSplash)
+
     area.damage(1)
     queue_free()
